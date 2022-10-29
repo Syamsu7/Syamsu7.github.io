23788dc2193d01da37deda4219cbcf2b1f88a758
Sam <108341453+Syamsu7@users.noreply.github.com>
2022-10-29 09:52:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+//ahow hide element
+
+function toggleElement(){
+	let x = document.getElementById('elemen');
+	if( x.style.display == 'none'){
+		x.style.display = 'block';
+	} else{
+		x.style.display = 'none';
+	}
+}
+
+function toggleElement2(){
+	let x = document.getElementById('elemen2');
+	if( x.style.display == 'none'){
+		x.style.display = 'block';
+	} else{
+		x.style.display = 'none';
+	}
+}
+
+function toggleElement3(){
+	let x = document.getElementById('elemen3');
+	if( x.style.display == 'none'){
+		x.style.display = 'block';
+	} else{
+		x.style.display = 'none';
+	}
+}
+
+function toggleElement4(){
+	let x = document.getElementById('elemen4');
+	if( x.style.display == 'none'){
+		x.style.display = 'block';
+	} else{
+		x.style.display = 'none';
+	}
+}
